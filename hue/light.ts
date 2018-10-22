39c4202d8769df2d096e861d08549d8dfe3a1371
Aiden Scott <aidenscott12@googlemail.com>
2018-10-22 22:33:42 +0100

    Update light interface

--- hue/light.ts
+++ hue/light.ts
@@ -2,6 +2,7 @@ type Light = {
     state: LightState,
     type: string,
     name: string 
+    // lightId: string, // A light should be in charge of its own id. need to make some transformation Process to actually get it here and not from the parent object
 }
 
 type LightState = {
