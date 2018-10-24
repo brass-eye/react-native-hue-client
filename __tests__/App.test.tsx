c377ba9aa3c143e6c96f5371cef328e2f8806dce
Aiden Scott <aidenscott12@googlemail.com>
2018-10-24 08:29:36 +0100

    Add some psuedocode test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme'
 import App from '../App';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
+
+// it('displays error when cant connect to hub')
+//   const app = shallow(<App hubIpAddress={'examplel.com'} apiKey={'wrong'}/>
+//   assert error message); 
+
+// it('displays error when cant authenticate')
+//   const app = shallow(<App hubIpAddress={'examplel.com'} apiKey={'wrong'}/>
+//   assert error message); 
+
+// it('displays all bulbs')
+//   const app = shallow(<App hubIpAddress={'mockl'} apiKey={'mock'}/>
+//   assert all the rendered bulbs matches the mock data); 
