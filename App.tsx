f1c0397a3868459d91c42ca046ff2f05313878d0
Aiden Scott <aidenscott12@googlemail.com>
2018-10-24 07:01:39 +0100

    Move generated test

--- App.tsx
+++ App.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Hello } from './components/Hello';
-import { debug } from 'util';
 import ToggleLightButtons from './components/ToggleLightButtons';
 
 type State = {
--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import App from './App';
-
 import * as renderer from 'react-test-renderer';
+import App from '../App';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
